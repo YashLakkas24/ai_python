323b17e3b3ced91e92ca47d0a9eae619bbd803c4
Yash Lakkas <yashnirjala24@gmail.com>
2026-07-13 14:38:42 +0530

    Test webhook

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from fastapi import FastAPI, File, UploadFile
 from typing import Annotated
 from pypdf import PdfReader
 
+#practice
 load_dotenv(override=True)
 app = FastAPI()
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
